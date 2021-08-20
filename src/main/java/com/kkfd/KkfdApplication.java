d699fade154d8ef2d576e9777629e6962b5c34e6
typhoon198 <typhoon198@naver.com>
2021-08-20 14:10:54 +0900

    #0 초기 설정, 마이바티스 설정

---
+++ src/main/java/com/kkfd/KkfdApplication.java
@@ -0,0 +1,13 @@
+package com.kkfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkfdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkfd/KkfdApplicationTests.java
@@ -0,0 +1,24 @@
+package com.kkfd;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class KkfdApplicationTests {
+
+	@Autowired
+	ApplicationContext context;
+
+	@Test
+	void contextLoads() {
+		assertNotNull(context.getBean("checkloginedController"));
+		assertNotNull(context.getBean("customerController"));
+		assertNotNull(context.getBean("downloadController"));
+		assertNotNull(context.getBean("repBoardController"));
+	}
+
+}

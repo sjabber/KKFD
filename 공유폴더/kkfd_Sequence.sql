c5e1eb8218a9f5ab6bf1185932ae01d21030ccdf
sjabber <sjabber91@gmail.com>
2021-08-21 15:20:59 +0900

    시퀀스 sql 구문 작성

--- 공유폴더/kkfd_Sequence.sql
+++ 공유폴더/kkfd_Sequence.sql
@@ -1,3 +1,29 @@
-project
-funding
-bookmark
+create sequence project
+increment by 1
+start with 1
+minvalue 1
+nocycle
+CACHE 20;
+
+CREATE SEQUENCE funding
+increment by 1
+start with 1
+minvalue 1
+nocycle
+cache 20;
+
+CREATE SEQUENCE bookmark
+increment by 1
+start with 1
+minvalue 1
+nocycle
+cache 20;
+
+-- funding 시퀀스의 다음값 확인
+SELECT funding.NEXTVAL
+FROM dual;
+
+-- funding 시퀀스의 다음값 변경
+-- 현재값-2 = 다음 시퀀스값 으로 맞춘다.
+ALTER SEQUENCE funding
+INCREMENT BY -2;

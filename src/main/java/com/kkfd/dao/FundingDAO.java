39d9935dded9f08f6a9177dfb0522025017a2d5f
typhoon198 <typhoon198@naver.com>
2021-08-29 02:44:25 +0900

    #10 펀딩참여자조회, 마이프로젝트 완성 (운송장입력, 취소)

--- src/main/java/com/kkfd/dao/FundingDAO.java
+++ src/main/java/com/kkfd/dao/FundingDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.kkfd.dto.FundingDTO;
 import com.kkfd.exception.AddException;
 import com.kkfd.exception.FindException;
+import com.kkfd.exception.ModifyException;
 
 public interface FundingDAO {
 	/**
@@ -22,4 +23,12 @@ public interface FundingDAO {
 	 * @throws FindException
 	 */
 	public List<FundingDTO> selectFunsByProjNo(int projNo, String loginId) throws FindException;
+	
+	/**
+	 * 
+	 * @param 펀딩번호,운송장번호가 담긴 펀딩 리스트
+	 * @return update된 행 수(rowCnt)
+	 * @throws ModifyException
+	 */
+	public int updateFuns(List<FundingDTO> list) throws ModifyException;
 }
--- src/main/java/com/kkfd/service/FundingService.java
+++ src/main/java/com/kkfd/service/FundingService.java
@@ -3,15 +3,12 @@ package com.kkfd.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.kkfd.dao.FundingDAO;
-import com.kkfd.dao.ProjectDAO;
 import com.kkfd.dto.FundingDTO;
-import com.kkfd.dto.ProjectDTO;
-import com.kkfd.dto.SearchDTO;
 import com.kkfd.exception.FindException;
+import com.kkfd.exception.ModifyException;
 
 @Service
 public class FundingService {
@@ -20,5 +17,9 @@ public class FundingService {
 
 	public List<FundingDTO> findFunsByProjNo(int projNo, String loginId) throws FindException {
 		return dao.selectFunsByProjNo(projNo, loginId);
+	}
+	
+	public int modifyFuns(List<FundingDTO> list) throws ModifyException {
+		return dao.updateFuns(list);
 	}
 }

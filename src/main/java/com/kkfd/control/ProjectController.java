49d8293432c3a3c877e378db0d745ab167741e13
sjabber <sjabber91@gmail.com>
2021-08-29 20:53:19 +0900

    날아간 내용들 복구 및 수정

--- src/main/java/com/kkfd/control/ProjectController.java
+++ src/main/java/com/kkfd/control/ProjectController.java
@@ -1,10 +1,9 @@
 package com.kkfd.control;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
+import com.kkfd.dto.ProjectDTO;
+import com.kkfd.dto.SearchDTO;
+import com.kkfd.exception.FindException;
+import com.kkfd.service.ProjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kkfd.dto.ProjectDTO;
-import com.kkfd.dto.SearchDTO;
-import com.kkfd.exception.FindException;
-import com.kkfd.service.ProjectService;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/project/*")
@@ -28,37 +26,37 @@ public class ProjectController {
 	private ProjectService service;
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	/*
-	 * 이벤트1 : 카테고리,정렬기준,상태,달성률 select값이 달라지면 
-	 * 이벤트2 : 스크롤을 내리면 		
-	 * 이벤트3 : 검색버튼을 누르면 	
+	 * 이벤트1 : 카테고리,정렬기준,상태,달성률 select값이 달라지면
+	 * 이벤트2 : 스크롤을 내리면
+	 * 이벤트3 : 검색버튼을 누르면
 	 *      이벤트 3-1 : 검색버튼을 누른 후 카테고리 바꾸면 검색+카테고리
-	*/
+	 */
 	@GetMapping(value={"/list/{page}"})
 	public ResponseEntity<List<ProjectDTO>> projList(HttpSession session,SearchDTO search, @PathVariable int page)
-			 throws FindException {
+			throws FindException {
 		//String loginId = (String)session.getAttribute("loginId");
 		search.setPage(page);
 		log.info(search.toString());
 		String loginId="id1";
 		if(loginId != null) {
 			search.setId(loginId);
-		} 
-		
+		}
+
 		//초안
 		try {
 			List<ProjectDTO> list = new ArrayList<ProjectDTO>();
 			list = service.findProjs(search);
-		/* 1.load시 요청  : list/?p=1
-		 * 2.카테고리,진행상태,달성률,정렬 	: /list/?p=1&category=1&state=2&goal=3&order=date>
-		 * 3.카테고리,진행상태,달성률,정렬+단어	: /list?p=1&category=0&state=0&goal=0&order=b&word=word
-		 */
+			/* 1.load시 요청  : list/?p=1
+			 * 2.카테고리,진행상태,달성률,정렬 	: /list/?p=1&category=1&state=2&goal=3&order=date>
+			 * 3.카테고리,진행상태,달성률,정렬+단어	: /list?p=1&category=0&state=0&goal=0&order=b&word=word
+			 */
 			return new ResponseEntity(list,null,HttpStatus.OK);//응답 변경	
 			//html에서 if(list==null) 목록이 존재하지 않습니다.
 		}catch(FindException e){
 			return new ResponseEntity(HttpStatus.BAD_GATEWAY);//응답 변경	   
 		}
 	}
-	
+
 	@GetMapping(value={"/{no}"})
 	public ResponseEntity<ProjectDTO> proj(HttpSession session, @PathVariable int no) {
 		//String loginId = (String)session.getAttribute("loginId");
@@ -71,7 +69,7 @@ public class ProjectController {
 		}
 		return null;
 	}
-	
+
 	@GetMapping(value={"/{no}/history"})
 	public ResponseEntity<List<ProjectDTO>> prevProj(@PathVariable int no) {
 		try {
@@ -83,6 +81,6 @@ public class ProjectController {
 		}
 		//return null;
 	}
-	
+
 }
 
--- src/main/webapp/resource/server_test.js
+++ src/main/webapp/resource/server_test.js
@@ -3,12 +3,40 @@ const fs = require('fs');
 const express = require('express')
     , http = require('http')
     , app = express()
+    , session = require('express-session')
+    , MemoryStore = require("memorystore")(session)
     , server = http.createServer(app);
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, './public')));
+app.use(session({
+    secret: 'secret key', // 암호화하는데 쓰일 키
+    // httpOnly: true, // 자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 한다.
+    // secure: true, // https 환경에서만 session 정보를 주고받도록 처리한다.
+    resave: false, // 세션을 언제나 저장할지 설정함
+    saveUninitialized: true, // 세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
+/*    store: new MemoryStore({
+        checkPeriod: 86400000, // 60 * 30 * 1000ms => 30분
+    }),*/
+    cookie: {
+        domain: true,
+        path: '/',
+        maxAge: 86400000,
+        sameSite: 'None',
+        httpOnly: true,
+        // Secure: true
+    }
+}));
 
 app.get('/', function (req, res) {
+/*    console.log(req.session);*/
+/*    if (req.session.num === undefined) {
+        req.session.num = 1;
+    } else {
+        req.session.num += 1;
+    }
+    res.send(`View : ${req.session.num}`);*/
+
     res.writeHead(200, {"Content-Type": "text/html"});
     fs.createReadStream("./index.html").pipe(res);
 });
@@ -28,15 +56,15 @@ app.get('/register/registerAgree', function (req, res) {
     fs.createReadStream("./register/registagree.html").pipe(res);
 });
 
-app.get('/register/regist/basicInfo', function (req, res) {
+app.get('/register/regist', function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     fs.createReadStream("./register/regist.html").pipe(res);
 });
 
-/*app.get('/register/regist/basicInfo', function (req, res) {
+app.get('/register/regist/basicInfo', function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     fs.createReadStream("./register/basicInfo.html").pipe(res);
-});*/
+});
 
 app.get('/register/regist/creatorInfo', function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});

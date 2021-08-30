68a7993ef28596bf7bcb6ccf35512f16b63a6617
ksy <bsc4443@gmail.com>
2021-08-30 11:59:05 +0900

    프로젝트 목록 구현 등

--- src/main/java/com/kkfd/control/FundingController.java
+++ src/main/java/com/kkfd/control/FundingController.java
@@ -1,10 +1,41 @@
 package com.kkfd.control;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kkfd.dto.FundingDTO;
+import com.kkfd.dto.MemberDTO;
+import com.kkfd.exception.AddException;
+import com.kkfd.service.FundingService;
 
 @RestController
 @RequestMapping("/funding/*")
 public class FundingController {
+	
+	@Autowired
+	private FundingService service;
+	
+	@PostMapping(value= {"/apply"})
+	public ResponseEntity<String> insertFunding (HttpSession session, @RequestBody FundingDTO funding) {
+		String loginId="id3";
+		MemberDTO m = new MemberDTO();
+		m.setMemId(loginId);
+		try {
+			service.addApply(funding);
+			return new ResponseEntity<String>(HttpStatus.OK);
+		} catch (AddException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
 
 }
--- src/main/java/com/kkfd/service/FundingService.java
+++ src/main/java/com/kkfd/service/FundingService.java
@@ -3,20 +3,21 @@ package com.kkfd.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.kkfd.dao.FundingDAO;
-import com.kkfd.dao.ProjectDAO;
 import com.kkfd.dto.FundingDTO;
-import com.kkfd.dto.ProjectDTO;
-import com.kkfd.dto.SearchDTO;
+import com.kkfd.exception.AddException;
 import com.kkfd.exception.FindException;
 
 @Service
 public class FundingService {
 	@Autowired
 	private FundingDAO dao;
+	
+	public void addApply(FundingDTO funding) throws AddException {
+		dao.insert(funding);
+	}
 
 	public List<FundingDTO> findFunsByProjNo(int projNo, String loginId) throws FindException {
 		return dao.selectFunsByProjNo(projNo, loginId);
